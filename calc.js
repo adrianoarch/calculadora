0b294e08d507580f8659e088d26c748532c7f764
Adriano de Oliveira <adriano.98@gmail.com>
2022-02-17 23:12:46 -0300

    Adicionando funcionalidade para dar o resultado apertando a tecla Enter e resetando o display com a tecla Esc

--- calc.js
+++ calc.js
@@ -1,3 +1,5 @@
+const displayCalculate = document.getElementById("inputValue").value;
+
 function insertValues(num) {
   const displayCalc = document.getElementById("inputValue").value;
   document.getElementById("inputValue").value = displayCalc + num;
@@ -18,7 +20,19 @@ function invertValue() {
   document.getElementById("inputValue").value = invert * -1;
 }
 
-function percentageValue(n1, n2){
- const percentageCalc = document.getElementById("inputValue").value
- document.getElementById("inputValue").value = eval(percentageCalc) / 100
+function percentageValue(n1, n2) {
+  const percentageCalc = document.getElementById("inputValue").value;
+  document.getElementById("inputValue").value = eval(percentageCalc) / 100;
 }
+
+document.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      document.getElementById("result").click();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      document.getElementById("reset").click();
+    }
+  });
